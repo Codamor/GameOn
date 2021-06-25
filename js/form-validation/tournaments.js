c8f3401cc01fc373102fba385272d2c15d861db8
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-06-25 22:31:46 +0200

    refactor checkTournaments functions

--- js/form-validation/tournaments.js
+++ js/form-validation/tournaments.js
@@ -1,3 +1,42 @@
+//FUNCTIONS TO DISPLAY MESSAGES
+
+function displayErrorNumberOfTournamentsNegative(){
+    numberOfTournamentsFormData
+        .setAttribute(["data-error-visible"], true) ;
+    numberOfTournamentsFormData
+        .setAttribute(["data-error"], "Ce nombre n'est pas valide") ;
+    numberOfTournamentsFormData
+        .removeAttribute(["data-valid-visible"]) ;
+}
+
+function displayErrorLocationsWithoutTournamentNumber(){
+    numberOfTournamentsFormData
+        .setAttribute(["data-error-visible"], true) ;
+    numberOfTournamentsFormData
+        .setAttribute(["data-error"], "Veuillez indiquer le nombre de tournois.") ;
+    numberOfTournamentsFormData
+        .removeAttribute(["data-valid-visible"]) ;
+}
+
+function displayErrorTournamentsNumberWithoutLocations(){
+    numberOfTournamentsFormData
+        .setAttribute(["data-error-visible"], true) ;
+    numberOfTournamentsFormData
+        .setAttribute(["data-error"], "Veuillez indiquer les villes correspondantes.") ;
+    numberOfTournamentsFormData
+        .removeAttribute(["data-valid-visible"]) ;
+}
+
+function tournamentsInputIsOk(){
+    numberOfTournamentsFormData
+        .removeAttribute(["data-error-visible"]) ;
+    numberOfTournamentsFormData
+        .removeAttribute(["data-error"]) ;
+    numberOfTournamentsFormData
+        .setAttribute(["data-valid-visible"], true) ;
+}
+
+//FUNCTION TO CHECK LOCATIONS
 function checkNumberOfLocationsSelected(){
 
     let numberOfLocationsSelected = 0 ;
@@ -24,12 +63,15 @@ function checkNumberOfLocationsSelected(){
     return numberOfLocationsSelected ;
 }
 
+//FUNCTION TO CHECK TOURNAMENTS NUMBER
 function checkNumberOfTournaments(){
 
     return Number(numberOfTournaments.value) ;
 
 }
 
+
+//FUNCTION TO CHECK TOURNAMENTS
 function checkTournaments(){
 
     let numberOfLocationsSelected = checkNumberOfLocationsSelected() ;
@@ -37,40 +79,22 @@ function checkTournaments(){
 
     if (numberOfTournaments < 0){
 
-        numberOfTournamentsFormData
-            .setAttribute(["data-error-visible"], true) ;
-        numberOfTournamentsFormData
-            .setAttribute(["data-error"], "Ce nombre n'est pas valide") ;
-        numberOfTournamentsFormData
-            .removeAttribute(["data-valid-visible"]) ;
+        displayErrorNumberOfTournamentsNegative() ;
+        return false ;
 
     } else if (numberOfTournaments === 0 && numberOfLocationsSelected > 0){
 
-        numberOfTournamentsFormData
-            .setAttribute(["data-error-visible"], true) ;
-        numberOfTournamentsFormData
-            .setAttribute(["data-error"], "Veuillez indiquer le nombre de tournois.") ;
-        numberOfTournamentsFormData
-            .removeAttribute(["data-valid-visible"]) ;
+        displayErrorLocationsWithoutTournamentNumber() ;
+        return false ;
 
     } else if (numberOfTournaments > 0 && numberOfLocationsSelected === 0){
 
-        numberOfTournamentsFormData
-            .setAttribute(["data-error-visible"], true) ;
-        numberOfTournamentsFormData
-            .setAttribute(["data-error"], "Veuillez indiquer les villes correspondantes.") ;
-        numberOfTournamentsFormData
-            .removeAttribute(["data-valid-visible"]) ;
+        displayErrorTournamentsNumberWithoutLocations() ;
+        return false ;
 
     } else if ((numberOfTournaments > 0 && numberOfLocationsSelected > 0) || (numberOfTournaments === 0 && numberOfLocationsSelected === 0)){
 
-        numberOfTournamentsFormData
-            .removeAttribute(["data-error-visible"]) ;
-        numberOfTournamentsFormData
-            .removeAttribute(["data-error"]) ;
-        numberOfTournamentsFormData
-            .setAttribute(["data-valid-visible"], true) ;
-
+        tournamentsInputIsOk() ;
         return true ;
     }
 }
