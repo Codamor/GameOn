632890de009865dafc8746c6bbeab7d0c2ce5727
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-06-25 22:56:05 +0200

    refacor email function

--- js/form-validation/email.js
+++ js/form-validation/email.js
@@ -1,24 +1,34 @@
-function checkEmail(){
+//FUNCTIONS TO DISPLAY MESSAGES
 
+function displayErrorEmailNotValid(){
+    emailFormData
+        .setAttribute(["data-error-visible"], true) ;
+    emailFormData
+        .setAttribute(["data-error"], "Email non valide.") ;
+    emailFormData
+        .removeAttribute(["data-valid-visible"]) ;
+}
+
+function emailIsValid(){
+    emailFormData
+        .removeAttribute(["data-error-visible"]) ;
+    emailFormData
+        .removeAttribute(["data-error"]) ;
+    emailFormData
+        .setAttribute(["data-valid-visible"], true) ;
+}
+
+//FUNCTION TO CHECK EMAIL
+function checkEmail(){
 
     if (inputIsEmail.test(email.value) === false){
 
-        emailFormData
-            .setAttribute(["data-error-visible"], true) ;
-        emailFormData
-            .setAttribute(["data-error"], "Email non valide.") ;
-        emailFormData
-            .removeAttribute(["data-valid-visible"]) ;
-
+        displayErrorEmailNotValid() ;
+        return false ;
 
     } else {
-        emailFormData
-            .removeAttribute(["data-error-visible"]) ;
-        emailFormData
-            .removeAttribute(["data-error"]) ;
-        emailFormData
-            .setAttribute(["data-valid-visible"], true) ;
 
+        emailIsValid() ;
         return true ;
     }
 }
