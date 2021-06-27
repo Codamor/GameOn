0c619535c5d07fbf9f8871b574693903f1a8415b
Adelino ARAUJO <araujo_adelino@orange.fr>
2021-06-27 17:26:57 +0200

    add submit-confirmation open/close modal functions

--- js/modal.js
+++ js/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground") ;
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll(".close");
+const modalSubmitConfirmation = document.getElementsByClassName("submit-confirmation")[0] ;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -29,5 +30,14 @@ closeBtn.forEach( (close) => close.addEventListener("click", closeModal)) ;
 function closeModal(){
   modalbg
       .setAttribute(["visible"], false) ;
+}
+
+//submit-confirmation modal
+function openSubmitConfirmationModal(){
+  modalSubmitConfirmation.setAttribute(["visible", true]);
+}
+
+function closeSubmitConfirmationModal(){
+  modalSubmitConfirmation.setAttribute(["visible", false]);
 }
 
